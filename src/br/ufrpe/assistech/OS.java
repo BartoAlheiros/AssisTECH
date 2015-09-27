54496ff24b1564248a15e9a4df105fbefef5af52
Michel Perilo <michel_perilo@hotmail.com>
2015-09-27 18:10:01 -0300

    Classe OS atualizada (+construtor)

--- src/br/ufrpe/assistech/OS.java
+++ src/br/ufrpe/assistech/OS.java
@@ -4,6 +4,15 @@ package br.ufrpe.assistech;
 
 public class OS {
     private int no;//número da OS
-    private String data;
-    private String entrega;
+    private double data;
+    private double entrega;
+
+    public OS(int no, double data, double entrega) { //construtor, aplicar Java.Time nas datas
+        no = no;
+        data = data;
+        entrega = entrega;
+    }
+    
+    
 }
+
