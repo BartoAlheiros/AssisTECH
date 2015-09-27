3f258a4d347ec58d77c6c1685648629718b8b4c0
alheirosb <alheirosb@alheirosb-Compaq-Presario-CQ42-Notebook-PC>
2015-09-26 23:26:11 -0300

    26/09

--- src/br/ufrpe/assistech/AssistechMain.java
+++ src/br/ufrpe/assistech/AssistechMain.java
@@ -1,9 +1,20 @@
 
 package br.ufrpe.assistech;
 
+import java.util.Scanner;
+
 
 public class AssistechMain {
     public static void main(String[] args){
-    
+        System.out.println("Bem vindo a AssisTECH!");
+        System.out.println("Escolha uma opcão: ");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("1 - Cadastrar Cliente.");
+        System.out.println("2 - Nova OS.");
+        
+        int opt = input.nextInt();
+        
+        
     }
 }
