ecadf3e2d74f63436a58cfa6cc41b720aa600494
Michel Perilo <michel_perilo@hotmail.com>
2015-09-27 18:27:26 -0300

    Classe Equip

---
+++ src/br/ufrpe/assistech/Equip.java
@@ -0,0 +1,25 @@
+
+package br.ufrpe.assistech;
+
+/**
+ *
+ * @author Michel
+ */
+public class Equip {
+    
+    public String marca;
+    public String modelo;
+    public String cor;
+    public int serie;
+    
+    public Equip (String marca, String modelo, String cor, int serie){
+        marca = marca;
+        modelo = modelo;
+        cor = cor;
+        serie = serie;
+    }
+    
+    public void cadastrarEquip(){
+        
+    }
+}
