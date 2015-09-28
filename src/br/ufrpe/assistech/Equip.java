0d0e7b0d8cf6ce1f353f981eb0ce0f3444678137
Michel Perilo <michel_perilo@hotmail.com>
2015-09-27 21:54:29 -0300

    Equip atualizada (métodos get/set)

--- src/br/ufrpe/assistech/Equip.java
+++ src/br/ufrpe/assistech/Equip.java
@@ -7,17 +7,11 @@ package br.ufrpe.assistech;
  */
 public class Equip {
     
-    public String marca;
-    public String modelo;
-    public String cor;
-    public int serie;
-    
-    public Equip (String marca, String modelo, String cor, int serie){
-        marca = marca;
-        modelo = modelo;
-        cor = cor;
-        serie = serie;
-    }
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int serie;
+    private Cliente cliente;
     
     public void cadastrarEquip(){
         
