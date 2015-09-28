fb86d83334b0ece19f19e2488df049e3263690b5
Michel Perilo <michel_perilo@hotmail.com>
2015-09-27 22:02:06 -0300

    OS atualizada (métodos get/set)

--- src/br/ufrpe/assistech/OS.java
+++ src/br/ufrpe/assistech/OS.java
@@ -3,16 +3,40 @@ package br.ufrpe.assistech;
 
 
 public class OS {
-    private int no;//número da OS
+    private String no;//número da OS
     private double data;
     private double entrega;
+    private Cliente cliente; 
 
-    public OS(int no, double data, double entrega) { //construtor, aplicar Java.Time nas datas
+    public OS(String no, double data, double entrega) { //construtor, aplicar Java.Time nas datas e gerador de número no NO
         this.no = no;
         this.data = data;
         this.entrega = entrega;
     }
-    
+  
+   public String getNo (){
+       return no;
+   }
+   
+   public double getData() {
+       return data;
+   }
+   
+   public double getEntrega(){
+       return entrega;
+   }
+   
+   public void setEntrega() {
+       this.entrega = entrega;
+   }
+   
+   public Cliente getCliente() {
+       return cliente;
+   }
+   
+    public String toString(){
+         
+    }
     
 }
 
